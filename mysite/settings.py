1bcf7d4a68c68dc3fff63df26961eb5ebd949c17
salam lakhan <119106199+salamlakhan7@users.noreply.github.com>
2026-01-11 00:24:58 +0500

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,8 @@ DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 ALLOWED_HOSTS = ['web-production-1e213.up.railway.app', 'localhost', '127.0.0.1']
 
 CSRF_TRUSTED_ORIGINS = ['https://web-production-1e213.up.railway.app']
-
+# This ensures Django knows it is behind a secure proxy (Railway)
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 # --- APPLICATION DEFINITION ---
 
 INSTALLED_APPS = [
