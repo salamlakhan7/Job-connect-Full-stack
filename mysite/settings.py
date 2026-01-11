64d54f137e0173e2af2eceb977301cb0e4fba828
salam lakhan <119106199+salamlakhan7@users.noreply.github.com>
2026-01-11 10:00:47 +0500

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -27,6 +27,10 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 CSRF_COOKIE_HTTPONLY = True
+CSRF_TRUSTED_ORIGINS = [
+    'https://web-production-1e213.up.railway.app',
+    'https://*.railway.app'
+]
 
 # SESSION_COOKIE_SECURE = True
 # CSRF_COOKIE_SECURE = True
